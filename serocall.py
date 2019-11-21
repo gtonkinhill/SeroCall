d7426915123294a6b3397e53b345bc00fa3479f2
Gerry Tonkin-Hill <gt4@sanger.ac.uk>
2019-11-21 15:28:38 +0000

    fix for python3

--- serocall.py
+++ serocall.py
@@ -3,8 +3,6 @@ import sys
 import argparse
 from itertools import groupby
 from math import log
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
 
 # Process the command-line arguments
 
